2dd1f449bbd6cf32b337886b85e8a9c0620e1794
Gabriel Galperin <galperin@alunos.utfpr.edu.br>
2024-01-18 17:37:02 -0300

    fix url to new path in github

--- predict-test.py
+++ predict-test.py
@@ -14,10 +14,9 @@ print(response) # expected response is Barrel_jellyfish
 
 
 image_data = {
-    "url": 'https://github.com/Nireplag/jellyfish_classification/raw/main/44.jpg',
+    "url": 'https://github.com/Nireplag/jellyfish_classification/raw/main/dataset/valid/Moon_jellyfish/44.jpg',
     "path": ''
 }
-
 
 response = requests.post(url, json=image_data).json()
 print(response) # expect response is Moon_jellyfish
